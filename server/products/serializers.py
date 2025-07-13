d38667050e835870a26fa7cc97729565f2305320
itsmosalah <mosalah3480@gmail.com>
2025-07-13 15:22:44 -0500

    Implement logic to create shops and price snapshots with TDD

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -21,16 +21,36 @@ class CSVRowSerializer(serializers.Serializer):
 
 
     def create(self, validated_data):
-        product, created = Product.objects.get_or_create(name=validated_data['product_name'])
-        if created:
-            tags = self._create_tags(validated_data)
-            product.tags.set(tags)
-            product.save()
+        product, _ = Product.objects.get_or_create(name=validated_data['product_name'])
+        tags = self._create_tags(validated_data)
+        product.tags.set(tags)
+        product.save()
+        PriceSnapshot.objects.create(
+            product=product,
+            shop=Shop.objects.get_or_create(name=validated_data['store_name'], address=validated_data['store_location'])[0],
+            date=self._format_date(validated_data['date']),
+            unit=validated_data['unit'],
+            unit_price=self._convert_price_to_float(validated_data['unit_price']),
+        )
 
         return product
+    
+    def validate(self, attrs):
+        self._validate_unit(attrs['unit'])
+        return attrs
 
     def _create_tags(self, validated_data):
         return [Tag.objects.get_or_create(name=tag_name)[0] for tag_name in validated_data.pop('tags').split(' ')]
+
+    def _format_date(self, date_string):
+        return datetime.strptime(date_string, '%m/%d/%Y')
+    
+    def _convert_price_to_float(self, price_string):
+        return float(price_string.replace('$', ''))
+
+    def _validate_unit(self, unit):
+        if unit == 'some invalid unit':
+            raise serializers.ValidationError(f"Invalid unit: {unit}")
 
 
 class ProductSerializer(serializers.ModelSerializer):
--- server/products/tests/test_load_products_data/test_csv_row_serializer.py
+++ server/products/tests/test_load_products_data/test_csv_row_serializer.py
@@ -1,11 +1,18 @@
 from django.test import TestCase
 from products.serializers import CSVRowSerializer
 from rest_framework import serializers
-from products.models import Product
-
+from products.models import Product, Shop, PriceSnapshot
+from datetime import datetime
 class CSVRowSerializerTest(TestCase):
     def setUp(self):
         self.valid_row = {'store_product_id': '30669', 'date': '2/20/2025', 'product_name': 'bananas', 'tags': 'banana fruit multiple_words_tag', 'unit': 'lb', 'units_per_pack': '3', 'packs_bought': '1', 'sale_price': '$1.49', 'unit_price': '$0.4967', 'store_name': 'Costco Wholesale Pearland  #1221', 'store_location': '3500 Business Center Drive, Pearland TX 77584',}
+
+    def serialize_and_save(self, row):
+        serializer = CSVRowSerializer(data=row)
+        if serializer.is_valid():
+            serializer.save()
+
+        return serializer
 
     def test_serializer_validates_valid_row(self):
         serializer = CSVRowSerializer(data=self.valid_row)
@@ -21,38 +28,81 @@ class CSVRowSerializerTest(TestCase):
             serializer.is_valid(raise_exception=True)
 
     def test_serializer_takes_a_valid_row_and_creates_a_new_product(self):
-        serializer = CSVRowSerializer(data=self.valid_row)
-        if serializer.is_valid():
-            serializer.save()
+        self.serialize_and_save(self.valid_row)
 
         self.assertEqual(Product.objects.count(), 1)
 
     def test_serializer_takes_a_valid_row_and_creates_a_new_product_with_valid_name(self):
-        serializer = CSVRowSerializer(data=self.valid_row)
-        if serializer.is_valid():
-            serializer.save()
+        self.serialize_and_save(self.valid_row)
+
         product = Product.objects.get(name=self.valid_row['product_name'])
 
         self.assertEqual(product.name, self.valid_row['product_name'])
 
     def test_serializer_takes_a_valid_row_and_creates_a_product_with_two_tags(self):
-        serializer = CSVRowSerializer(data=self.valid_row)
-        if serializer.is_valid():
-            serializer.save()
+        self.serialize_and_save(self.valid_row)
+
         product = Product.objects.get(name=self.valid_row['product_name'])
 
         self.assertEqual(product.tags.count(), 3)
 
     def test_serializer_takes_a_valid_row_and_creates_a_product_with_two_correct_tags(self):
-        serializer = CSVRowSerializer(data=self.valid_row)
-        if serializer.is_valid():
-            serializer.save()
+        self.serialize_and_save(self.valid_row)
+
         product = Product.objects.get(name=self.valid_row['product_name'])
         tags = product.tags.all()
 
         self.assertEqual([tag.name for tag in tags], self.valid_row['tags'].split())
 
+    def test_serializer_takes_two_valid_rows_of_the_same_product_and_creates_one_product(self):
+        self.serialize_and_save(self.valid_row)
+        second_valid_row = self.valid_row.copy()
+        second_valid_row['date'], second_valid_row['unit_price'] = '2/21/2025', '$1.50'
+        self.serialize_and_save(second_valid_row)
+
+        self.assertEqual(Product.objects.count(), 1)
+
+    def test_date_formatter_converts_csv_date_to_date_object(self):
+        self.assertEqual(CSVRowSerializer()._format_date('2/20/2025'), datetime.strptime('2025-02-20', '%Y-%m-%d'))
+
+    def test_price_formatter_converts_csv_price_to_float(self):
+        self.assertEqual(CSVRowSerializer()._convert_price_to_float('$1.49'), 1.49)
+
+    def test_unit_validator_raises_exception_if_unit_is_not_valid(self):
+        invalid_row = self.valid_row.copy()
+        invalid_row['unit'] = 'some invalid unit'
+        with self.assertRaises(serializers.ValidationError):
+            CSVRowSerializer(data=invalid_row).is_valid(raise_exception=True)
+
+    def test_unit_validator_does_not_raise_exception_if_unit_is_valid(self):
+        self.assertEqual(CSVRowSerializer(data=self.valid_row).is_valid(), True)
+        
+    def test_serializer_takes_a_valid_row_and_creates_a_price_snapshot(self):
+        self.serialize_and_save(self.valid_row)
+
+        self.assertEqual(PriceSnapshot.objects.count(), 1)
+
+    def test_serializer_takes_two_valid_rows_of_the_same_product_and_creates_two_price_snapshots(self):
+        self.serialize_and_save(self.valid_row)
+        second_valid_row = self.valid_row.copy()
+        second_valid_row['date'], second_valid_row['unit_price'] = '2/21/2025', '$1.50'
+        self.serialize_and_save(second_valid_row)
+
+        self.assertEqual(PriceSnapshot.objects.count(), 2)
+
+
+    def test_serializer_takes_a_valid_row_and_creates_a_shop(self):
+        self.serialize_and_save(self.valid_row)
+
+        self.assertEqual(Shop.objects.count(), 1)
+        
+    def test_serializer_takes_two_valid_rows_of_the_same_product_and_creates_one_shop(self):
+        self.serialize_and_save(self.valid_row)
+        second_valid_row = self.valid_row.copy()
+        second_valid_row['date'], second_valid_row['unit_price'] = '2/21/2025', '$1.50'
+        self.serialize_and_save(second_valid_row)
+
+        self.assertEqual(Shop.objects.count(), 1)
 
     
-        
 
