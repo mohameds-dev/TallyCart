4dbf4def2e11d50533e230eb422aef57bb86948c
itsmosalah <mosalah3480@gmail.com>
2025-07-19 19:40:51 -0500

    Add search feature to products with tests
    - Add search filter to view with name and tags
    - Add the api call samples to products.http
    
    ref #2

--- server/products/tests/test_views.py
+++ server/products/tests/test_views.py
@@ -4,7 +4,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from rest_framework import status
 
-class TestProductViews(TestCase):
+class TestProductRetrievalViews(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.url = reverse('products')
@@ -40,3 +40,56 @@ class TestProductViews(TestCase):
         response = self.client.get(self.url)
         
         self.assertEqual(response.data[0]['tags'][0], {'id': 1, 'name': 'TestTag'})
+
+class TestProductSearchViews(TestCase):
+    def setUp(self):    
+        self.client = APIClient()
+        self.url = reverse('products')
+
+    def test_create_a_product_with_tags_and_search_by_name_returns_result_with_one_product(self):
+        Product.objects.create(name="Test Product")
+        response = self.client.get(self.url, {'search': 'Test Product'})
+
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_a_product_with_tags_and_search_by_incorrect_name_returns_empty_result(self):
+        Product.objects.create(name="Test Product")
+        response = self.client.get(self.url, {'search': 'Incorrect Name'})
+
+        self.assertEqual(len(response.data), 0)
+
+    def test_create_a_product_with_tags_and_search_by_tag_returns_result_with_one_product(self):
+        product = Product.objects.create(name="Test Product")
+        product.tags.add(Tag.objects.create(name="TestTag"))
+        response = self.client.get(self.url, {'search': 'TestTag'})
+
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_two_products_with_tags_and_search_by_name_returns_correct_products(self):
+        product1 = Product.objects.create(name="Test Product 1")
+        product2 = Product.objects.create(name="Test Product 2")
+        tag = Tag.objects.create(name="TestTag")
+        product1.tags.add(tag)
+        product2.tags.add(tag)
+        response = self.client.get(self.url, {'search': 'TestTag'})
+
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_two_products_with_tags_and_search_by_incorrect_tag_returns_empty_result(self):
+        product1 = Product.objects.create(name="Test Product 1")
+        product2 = Product.objects.create(name="Test Product 2")
+        product1.tags.add(Tag.objects.create(name="TestTag1"))
+        product2.tags.add(Tag.objects.create(name="TestTag2"))
+        response = self.client.get(self.url, {'search': 'Incorrect Tag'})
+
+        self.assertEqual(len(response.data), 0)
+
+    def test_create_two_products_with_tags_and_search_by_tag_returns_correct_products(self):
+        product1 = Product.objects.create(name="Test Product 1")
+        product1.tags.add(Tag.objects.create(name="TestTag1"))
+        product2 = Product.objects.create(name="Test Product 2")
+        product2.tags.add(Tag.objects.create(name="TestTag2"))
+        response = self.client.get(self.url, {'search': 'TestTag1'})
+
+        self.assertEqual(len(response.data), 1)
+
--- server/products/views.py
+++ server/products/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'tags__name']
