88808f1540eee1fec69f05513add8e019fda562c
mohamedsdev <mosalah3480@gmail.com>
2026-02-28 11:08:17 -0600

    Fix gemini API call and use improve the prompt

--- server/receipt_processor/llm/gemini_api.py
+++ server/receipt_processor/llm/gemini_api.py
@@ -5,8 +5,7 @@ GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 def get_response(prompt):
-    response = client.models.generate_content(
-        model='gemini-2.0-flash',
+    return client.models.generate_content(
+        model='gemini-2.5-flash',
         contents=prompt
     )
-    return response.text
